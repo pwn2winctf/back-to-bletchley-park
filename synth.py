133adc200e70ee35424cb13657d2a99f673c68bc
Paulo Matias <matias@ufscar.br>
2017-10-15 01:43:05 -0200

    Refatoring.. sub now ok

--- synth.py
+++ synth.py
@@ -1,233 +1,238 @@
-LIB = """
+qasm_code = ["""
 include "qelib1.inc";
-gate maj c,b,a
-{
-  cx a,b;
-  cx a,c;
-  ccx b,c,a;
-}
-gate ums c,s,a
-{
-  ccx s,c,a;
-  cx a,c;
-  cx c,s;
-}
-gate umj c,b,a
-{
-  ccx b,c,a;
-  cx a,c;
-  cx a,b;
-}
-gate cmj c,b,a,x
-{
-  ccx x,a,b;
-  cx a,c;
-  ccx b,c,a;
-}
-gate cus c,s,a,x
-{
-  ccx s,c,a;
-  cx a,c;
-  ccx x,c,s;
-}
-"""
+"""]
+
+
+gates_declared = set()
+
+
+class declare:
+    class AlreadyDeclared(Exception):
+        pass
+
+    class Bomb:
+        def __init__(self, exc):
+            self.exc = exc
+        def __getattr__(self, attr):
+            raise self.exc()
+
+    def __init__(self, name, params):
+        self.name = name
+        self.src = [
+            'gate {name} {params}'.format(name=name, params=params),
+            '{'
+        ]
+
+    def __enter__(self):
+        if self.name in gates_declared:
+            return self.Bomb(self.AlreadyDeclared)
+        gates_declared.add(self.name)
+        return self.src
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if exc_type == self.AlreadyDeclared:
+            return True
+        self.src.append('}')
+        qasm_code.extend(self.src)
 
 
 def arg_list(name, n):
-    """
-    Argument list
-
-    n qubits
-    """
     return ','.join('{}{}'.format(name, i) for i in range(n))
 
 
 def arg_vec(name, n):
-    """
-    Pass register as argument
-
-    n qubits
-    """
     return ','.join('{}[{}]'.format(name, i) for i in range(n))
 
 
-def cdkm(n):
-    """
-    The Complete Adder
-
-    input: b,a
-    output: b+a,a
-
-    a[n-1] is carry-in
-    b[n-1] is carry-out and must be |0> at input
-    """
-    src = 'gate cdkm{n} {b},{a}\n'.format(n=n,
-                                          b=arg_list('b',n),
-                                          a=arg_list('a',n))
-    src += '{\n'
-    for i in range(n-1):
-        src += '  maj a{},b{},a{};\n'.format((n + i - 1) % n, i, i)
-    src += '  cx a{},b{};\n'.format(n-2, n-1)
-    for i in range(n-2, -1, -1):
-        src += '  ums a{},b{},a{};\n'.format((n + i - 1) % n, i, i)
-    src += '}\n'
-    return src
+def maj():
+    with declare('maj', 'c,b,a') as src:
+        src.extend([
+            '  cx a,b;',
+            '  cx a,c;',
+            '  ccx b,c,a;',
+        ])
 
 
-def ccdkm(n):
-    """
-    Controlled CDKM Adder
+def ums():
+    with declare('ums', 'c,s,a') as src:
+        src.extend([
+            '  ccx s,c,a;',
+            '  cx a,c;',
+            '  cx c,s;',
+        ])
 
-    input: b,a,x
-    output: b+a*x,a,x
 
-    a[n-1] is carry-in
-    b[n-1] is carry-out and must be |0> at input
-    x is control
-    """
-    src = 'gate ccdkm{n} {b},{a},x\n'.format(n=n,
-                                             b=arg_list('b',n),
-                                             a=arg_list('a',n))
-    src += '{\n'
-    for i in range(n-1):
-        src += '  cmj a{},b{},a{},x;\n'.format((n + i - 1) % n, i, i)
-    src += '  cx a{},b{};\n'.format(n-2, n-1)
-    for i in range(n-2, -1, -1):
-        src += '  cus a{},b{},a{},x;\n'.format((n + i - 1) % n, i, i)
-    src += '}\n'
-    return src
+def umj():
+    with declare('umj', 'c,b,a') as src:
+        src.extend([
+            '  ccx b,c,a;',
+            '  cx a,c;',
+            '  cx a,b;',
+        ])
+
+
+def cmj():
+    with declare('cmj', 'c,b,a,x') as src:
+        src.extend([
+            '  ccx x,a,b;',
+            '  cx a,c;',
+            '  ccx b,c,a;',
+        ])
+
+
+def cus():
+    with declare('cus', 'c,s,a,x') as src:
+        src.extend([
+            '  ccx s,c,a;',
+            '  cx a,c;',
+            '  ccx x,c,s;',
+        ])
+
+
+def add(n):
+    """ CDKM adder without carry output """
+    maj()
+    ums()
+    with declare('add{}'.format(n),
+                 '{b},cin,{a}'.format(b=arg_list('b',n),
+                                      a=arg_list('a',n))) as src:
+        for i in range(n):
+            cin = 'cin' if i == 0 else 'a{}'.format(i - 1)
+            src.append('  maj {},b{},a{};'.format(cin, i, i))
+        for i in range(n-1, -1, -1):
+            cin = 'cin' if i == 0 else 'a{}'.format(i - 1)
+            src.append('  ums {},b{},a{};'.format(cin, i, i))
+
+
+def addc(n):
+    """ CDKM adder with carry output """
+    maj()
+    ums()
+    with declare('addc{}'.format(n),
+                 'cout,{b},cin,{a}'.format(b=arg_list('b',n),
+                                           a=arg_list('a',n))) as src:
+        for i in range(n):
+            cin = 'cin' if i == 0 else 'a{}'.format(i - 1)
+            src.append('  maj {},b{},a{};'.format(cin, i, i))
+        src.append('  cx a{},cout;'.format(n-2))
+        for i in range(n-1, -1, -1):
+            cin = 'cin' if i == 0 else 'a{}'.format(i - 1)
+            src.append('  ums {},b{},a{};'.format(cin, i, i))
+
+
+def cmb(n):
+    maj()
+    umj()
+    """ Comparator base block (just like addc but only changes cout) """
+    with declare('cmb{}'.format(n),
+                 'cout,{b},cin,{a}'.format(b=arg_list('b',n),
+                                           a=arg_list('a',n))) as src:
+        for i in range(n):
+            cin = 'cin' if i == 0 else 'a{}'.format(i - 1)
+            src.append('  maj {},b{},a{};'.format(cin, i, i))
+        src.append('  cx a{},cout;'.format(n-2))
+        for i in range(n-1, -1, -1):
+            cin = 'cin' if i == 0 else 'a{}'.format(i - 1)
+            src.append('  umj {},b{},a{};'.format(cin, i, i))
+
+
+def cadd(n):
+    """ Controlled CDKM adder without carry output """
+    cmj()
+    cus()
+    with declare('cadd{}'.format(n),
+                 '{b},cin,{a},x'.format(b=arg_list('b',n),
+                                        a=arg_list('a',n))) as src:
+        for i in range(n):
+            cin = 'cin' if i == 0 else 'a{}'.format(i - 1)
+            src.append('  cmj {},b{},a{},x;'.format(cin, i, i))
+        for i in range(n-1, -1, -1):
+            cin = 'cin' if i == 0 else 'a{}'.format(i - 1)
+            src.append('  cus {},b{},a{},x;'.format(cin, i, i))
+
+
+def caddc(n):
+    """ Controlled CDKM adder with carry output """
+    cmj()
+    cus()
+    with declare('caddc{}'.format(n),
+                 'cout,{b},cin,{a},x'.format(b=arg_list('b',n),
+                                             a=arg_list('a',n))) as src:
+        for i in range(n):
+            cin = 'cin' if i == 0 else 'a{}'.format(i - 1)
+            src.append('  cmj {},b{},a{},x;'.format(cin, i, i))
+        src.append('  cx a{},cout;'.format(n-2))
+        for i in range(n-1, -1, -1):
+            cin = 'cin' if i == 0 else 'a{}'.format(i - 1)
+            src.append('  cus {},b{},a{},x;'.format(cin, i, i))
 
 
 def plusone(n):
-    """
-    Plus 1
-
-    input: b,s
-    output: b+1,s
-
-    b[n-1] is carry-out and must be |0> at input
-    s is scratch and must be |0..0>
-    """
-    src = 'gate plusone{n} {b},{s}\n'.format(n=n,
-                                             b=arg_list('b',n),
-                                             s=arg_list('s',n))
-    src += '{\n'
-    src += '  x s0;\n'
-    src += '  cdkm{} {},{};\n'.format(n, arg_list('b',n), arg_list('s',n))
-    src += '  x s0;\n'
-    src += '}\n'
-    return src
+    add(n)
+    with declare('plusone{}'.format(n),
+                 '{b},{s}'.format(b=arg_list('b',n),
+                                  s=arg_list('s',n+1))) as src:
+        src.append('  x s0;')
+        src.append('  add{n} {b},s{n},{s};'.format(n=n,
+                                                   b=arg_list('b',n),
+                                                   s=arg_list('s',n)))
+        src.append('  x s0;')
 
 
 def minusone(n):
-    """
-    Minus 1
-
-    input: b,s
-    output: b-1,s
-
-    b[n-1] is carry-out and must be |0> at input
-    s is scratch and must be |0..0>
-    """
-    src = 'gate minusone{n} {b},{s}\n'.format(n=n,
-                                              b=arg_list('b',n),
-                                              s=arg_list('s',n))
-    src += '{\n'
-    for i in range(n-1):
-        src += '  x s{};\n'.format(i)
-    src += '  cdkm{} {},{};\n'.format(n, arg_list('b',n), arg_list('s',n))
-    for i in range(n-1):
-        src += '  x s{};\n'.format(i)
-    src += '  x b{};\n'.format(n-1)
-    src += '}\n'
-    return src
+    add(n)
+    with declare('minusone{}'.format(n),
+                 '{b},{s}'.format(b=arg_list('b',n),
+                                  s=arg_list('s',n+1))) as src:
+        for i in range(n):
+            src.append('  x s{};'.format(i))
+        src.append('  add{n} {b},s{n},{s};'.format(n=n,
+                                                   b=arg_list('b',n),
+                                                   s=arg_list('s',n)))
+        for i in range(n-1, -1, -1):
+            src.append('  x s{};'.format(i))
 
 
-def cdkmsub(n):
-    """
-    CDKM Subtractor
-    Known not to work with a==0
-
-    input: b,a,s
-    output: b-a,a,s
-
-    a[n-1] is carry-in
-    b[n-1] is carry-out and must be |0> at input
-    s is scratch and must be |0..0>
-    x is control
-    """
-    src = 'gate cdkmsub{n} {b},{a},{s}\n'.format(n=n,
-                                                 b=arg_list('b',n),
-                                                 a=arg_list('a',n),
-                                                 s=arg_list('s',n))
-    src += '{\n'
-    for i in range(n-1):
-        src += '  x a{};\n'.format(i)
-    src += '  plusone{} {},{};\n'.format(n, arg_list('a',n), arg_list('s',n))
-    src += '  cdkm{} {},{};\n'.format(n, arg_list('b',n), arg_list('a',n))
-    src += '  x b{};\n'.format(n-1)
-    src += '  minusone{} {},{};\n'.format(n, arg_list('a',n), arg_list('s',n))
-    for i in range(n-1):
-        src += '  x a{};\n'.format(i)
-    src += '}\n'
-    return src
-
-
-def ccdkmsub(n):
-    """
-    Controlled CDKM Subtractor
-    Known not to work with a==0
-
-    input: b,a,s,x
-    output: b-a,a,s,x
-
-    a[n-1] is carry-in
-    b[n-1] is carry-out and must be |0> at input
-    s is scratch and must be |0..0>
-    """
-    src = 'gate ccdkmsub{n} {b},{a},{s},x\n'.format(n=n,
-                                                    b=arg_list('b',n),
-                                                    a=arg_list('a',n),
-                                                    s=arg_list('s',n))
-    src += '{\n'
-    for i in range(n-1):
-        src += '  x a{};\n'.format(i)
-    src += '  plusone{} {},{};\n'.format(n, arg_list('a',n), arg_list('s',n))
-    src += '  ccdkm{} {},{},x;\n'.format(n, arg_list('b',n), arg_list('a',n))
-    src += '  cx x, b{};\n'.format(n-1)
-    src += '  minusone{} {},{};\n'.format(n, arg_list('a',n), arg_list('s',n))
-    for i in range(n-1):
-        src += '  x a{};\n'.format(i)
-    src += '}\n'
-    return src
+def sub(n):
+    plusone(n)
+    add(n)
+    minusone(n)
+    with declare('sub{}'.format(n),
+                 '{b},{a},{s}'.format(b=arg_list('b',n),
+                                      a=arg_list('a',n),
+                                      s=arg_list('s',n+1))) as src:
+        for i in range(n):
+            src.append('  x a{};'.format(i))
+        src.append('  plusone{n} {a},{s};'.format(n=n,
+                                                  a=arg_list('a',n),
+                                                  s=arg_list('s',n+1)))
+        src.append('  add{n} {b},s{n},{a};'.format(n=n,
+                                                   b=arg_list('b',n),
+                                                   a=arg_list('a',n)))
+        src.append('  minusone{n} {a},{s};'.format(n=n,
+                                                   a=arg_list('a',n),
+                                                   s=arg_list('s',n+1)))
+        for i in range(n-1, -1, -1):
+            src.append('  x a{};'.format(i))
 
 
 def synth():
-    src = LIB
-    src += cdkm(4)
-    src += ccdkm(4)
-    src += plusone(4)
-    src += minusone(4)
-    src += cdkmsub(4)
-    src += ccdkmsub(4)
-    src += """
-    qreg a[4];
+    sub(4)
+    qasm_code.append("""
     qreg b[4];
-    qreg scratch[4];
-    qreg ctrl[1];
+    qreg a[4];
+    qreg scratch[5];
     creg c[4];
 
-    //x ctrl[0];
-
-    x b[2];
-    x a[1];
+    //x b[0];
+    //x b[1];
     x a[0];
 
-    ccdkmsub4 {},{},{},ctrl[0];
+    sub4 {b},{a},{s};
 
     measure b -> c;
-    """.format(arg_vec('b',4),arg_vec('a',4),arg_vec('scratch',4))
-    return src
+    """.format(b=arg_vec('b',4),a=arg_vec('a',4),s=arg_vec('scratch',5),))
+    return '\n'.join(qasm_code)
 
 
 if __name__ == '__main__':
